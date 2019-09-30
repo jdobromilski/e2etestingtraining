fea1e8742e8795617745ea644a054cda2f06a9f6
jdobromilski <jaroslaw.dobromilski@gmail.com>
2019-09-30 14:21:48 +0100

    added .gitignore

--- e2etests/navigateToHomepage_test.js
+++ e2etests/navigateToHomepage_test.js
@@ -2,12 +2,12 @@ Feature('navigateToAEVIDev');
 
 Scenario('navigate to the AEVI developer homepage', (I) => {
     I.amOnPage('https://developer.aevi.com/');
-    I.see('Let’s create an awesome merchant experience'); 
+    I.see('Let’s create a better merchant experience'); 
     // broken test on purpose - fix it!
 });
 
 Scenario('navigate to the AEVI developer homepage', (I) => {
     I.amOnPage('https://developer.aevi.com/');
     I.click('Sign up');
-    I.see('Terms of Use for AEVI Websites'); 
+    I.see('Terms of Use for AEVI Websites');
 });
